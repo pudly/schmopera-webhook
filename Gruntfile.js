915f7798574fff96762ec660a94f077ebe100c49
Andrej <andrej@pud.ca>
2018-05-24 21:13:12 -0400

    Sass build task
    Added sasss build task into webhook config

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,40 +16,42 @@ module.exports = function(grunt) {
   // You have been warned!
   grunt.initConfig({
     // BUILD SASS TO CSS
-//    sass: {
-//      dev: {
-//        options: {
-//          // We used the expanded style because Webhook already minifies CSS when it deploys your site.
-//          style: 'expanded',
-//
-//          // Uncomment the below line to include outside directories as well.
-//          // loadPath: ['location/of/other/sass']
-//        },
-//        files: [{
-//          // Files in the /sass/ directory will go to /static/css/ when processed.
-//          expand: true,
-//          cwd: 'sass',
-//          src: ['schmopera.scss'],
-//          dest: 'static/css',
-//          ext: '.css'
-//        }]
-//      }
-//    },
-//      
-//    // WHEN FILES CHANGE, RUN THE ABOVE TASKS ALONG WITH BUILD
-//    watch: {
-//      sass : {
-//        files: ['sass/**/*.scss'],
-//        tasks: ['sass', 'build']
-//      }
-//    }
+   sass: {
+     dev: {
+       options: {
+         // We used the expanded style because Webhook already minifies CSS when it deploys your site.
+         style: 'expanded',
+
+         // Uncomment the below line to include outside directories as well.
+         // loadPath: ['location/of/other/sass']
+       },
+       files: [{
+         // Files in the /sass/ directory will go to /static/css/ when processed.
+         expand: true,
+         cwd: 'sass',
+         src: ['schmopera.scss'],
+         dest: 'static/css',
+         ext: '.css'
+       }]
+     }
+   },
+     
+   // WHEN FILES CHANGE, RUN THE ABOVE TASKS ALONG WITH BUILD
+
+   watch: {
+     sass : {
+       files: ['sass/**/*.scss'],
+       tasks: ['sass', 'build']
+     }
+   }
   });
 
   // THIS LOADS THE TASKS WE NEED ABOVE IN FROM OUR NPM
   // Note, that we need to have these installed through the package.json file as well
-//  grunt.loadNpmTasks('grunt-contrib-sass');    
+  grunt.loadNpmTasks('grunt-contrib-sass');    
     
   // NEVER REMOVE THESE LINES, OR ELSE YOUR PROJECT MAY NOT WORK
   require('./options/generatorOptions.js')(grunt);
   grunt.loadTasks('tasks');
+  
 };
